918b79a7fbdf34e6d891bcab86fb3bcfba955eb5
Johnny <johnnyliao06@gmail.com>
2020-11-21 04:06:08 -0800

    added routes to index

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes.BasicGet);
+app.use(routes.BasicPost);
+app.use(routes.BasicPut);
+app.use(routes.BasicDelete);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
