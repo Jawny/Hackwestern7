4a091d8485ec213bf6c3497db8cd9797b0110b03
CanFISHfly1 <nathanlu_@hotmail.com>
2020-11-21 15:27:15 -0800

    Added some comments

--- routes/Balance.js
+++ routes/Balance.js
@@ -3,14 +3,15 @@ const postRoute = express.Router();
 const BlockchainHandler = require('../scripts/blockchain');
 require('dotenv').config({path: '../.env'});
 
+// @route POST api/balance
+// @desc Retrieves balance information of account provided by phone number
+// @access Public
 module.exports = postRoute.post("/balance", (req, res) => {
-	/*
-  const obj = { response: "we did it!" };
-  res.json(obj);
-	*/
 
+	// Initialize connection to blockchain
 	const b = new BlockchainHandler(process.env.ADDRESS);
 	b.init(async () => {
 		res.json(await b.readUser(0));
 	});
+
 });
